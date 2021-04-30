8b79ece60e2329a13ec93c5729e82ca7fdba8bf1
Frank Raúl Pérez Pérez <frpp9512@outlook.com>
2021-04-30 15:10:03 -0400

    Included the YP Security project inside the solution.

---
+++ YpSecurity/AuthInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Security;
+
+namespace YpSecurity
+{
+    public class AuthInfo
+    {
+        public AuthInfo(string host, string db_name, string user, string password)
+        {
+            this.Host = host;
+            this.DB_Name = db_name;
+            this.User = user;
+            this.Password = SecurityUtil.SecureString(password);
+            SecurityUtil.ReleaseFromMemory(ref password);
+        }
+
+        public string Host { get; }
+
+        public string DB_Name { get; }
+
+        public string User { get; }
+
+        public SecureString Password { get; }
+    }
+}
---
+++ YpSecurity/LogonEventArgs.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace YpSecurity
+{
+    public class LogonEventArgs : EventArgs
+    {
+        public LogonEventArgs(int logonTimes, DateTime logonAuthDateTime)
+        {
+            LogonTimes = logonTimes;
+            AuthDateTime = logonAuthDateTime;
+        }
+
+        public int LogonTimes { get; }
+
+        public DateTime AuthDateTime { get; }
+    }
+}
